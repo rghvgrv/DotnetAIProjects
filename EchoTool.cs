41a0cebb89612aff6a77f92ded5be4c842c4731f
Gaurav Ashish <71788323+rghvgrv@users.noreply.github.com>
2025-08-14 20:03:05 +0530

    MCP Server Setup

---
+++ EchoTool.cs
@@ -0,0 +1,12 @@
+﻿using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+namespace MCPServer
+{
+    [McpServerToolType]
+    public static class EchoTool
+    {
+        [McpServerTool,Description("TestingTool")]
+        public static string Echo(string msg) => $"Hello {msg}";
+    }
+}
---
+++ MCPServer.Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using ModelContextProtocol.Client;
+using ModelContextProtocol.Protocol;
+
+var client = new HttpClient();
+var uri = "http://localhost:3001";
+
+var transport = new SseClientTransport(new()
+{
+    Endpoint = new Uri(uri),
+    Name = "Secure Client",
+}, client);
+
+var clientMCP = await McpClientFactory.CreateAsync(transport);
+
+var tools = await clientMCP.ListToolsAsync(); // For Listing all the tools
+
+var results = await clientMCP.CallToolAsync(
+    "echo",
+    new Dictionary<string, object?> { { "msg", "World" } }
+    );
+
+Console.WriteLine("Results : " + ((TextContentBlock)results.Content[0]).Text);
